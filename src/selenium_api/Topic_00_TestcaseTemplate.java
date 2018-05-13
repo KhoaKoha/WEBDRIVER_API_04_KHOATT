53a0b2b3e1f54f1ec22cff553be54c31bf049470
Store <Store@DESKTOP-2RK6IFE>
2018-05-14 00:18:48 +0700

    First commit

---
+++ src/selenium_api/Topic_00_TestcaseTemplate.java
@@ -0,0 +1,38 @@
+package selenium_api;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_00_TestcaseTemplate {
+	WebDriver driver;
+
+	@BeforeClass
+	public void beforeClass() {		
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get("http://live.guru99.com/");
+	}
+
+	@Test
+	public void TC_01() {
+		
+	}
+	
+	@Test
+	public void TC_02() {
+		
+	}
+	
+	@AfterClass
+	public void afterClass() {
+		// quit browser
+		driver.quit();
+	}
+}
